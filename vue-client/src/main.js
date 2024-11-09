dbc6e02a1fd7b373ce2c039785dae5bdb4d572b2
Saeba Ryo <brahim.bakkas123@gmail.com>
2024-11-09 21:12:26 +0100

    * Installed and configured Fortify * Set up the project frontend stach (vue3 + vite + tailwind + primevue ) couupled with laravel api * Set the initial project skeleton to be cleaned later * Set and testes the login functionality (to be altered later)

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -1,14 +1,29 @@
+import './assets/tailwind.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from "primevue/config";
+import useAuth from './composables/useAuth'
+
+const { attempt } = useAuth()
+
+axios.defaults.baseURL = 'http://ryo-advocate.test'
+axios.defaults.withCredentials = true
+axios.defaults.withXSRFToken = true
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    theme: 'none'
+});
 
+attempt().then(() => {
 app.mount('#app')
+})
